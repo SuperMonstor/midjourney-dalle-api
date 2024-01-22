67e0351d795005e4a6fae4e6581a3e9ce350acfd
Sudarshan S <senthilsudarshan@gmail.com>
2024-01-23 00:23:25 +0530

    init

---
+++ client/ai-image-gen/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => {
+	return (
+    <BrowserRouter>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]" >
+        
+      </header>
+    </BrowserRouter>
+  );
+};
+
+export default App;
